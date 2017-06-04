84365e8dee379555bd56fe4091f98bcefe169911
sybrenboland <sybboland@gmail.com>
2017-06-04 13:06:05 -0700

    CSD3: Enable running docker-compose (#1)

--- src/main/java/demo/InventoryApplication.java
+++ src/main/java/demo/InventoryApplication.java
@@ -2,11 +2,11 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableHystrix
 public class InventoryApplication {
     public static void main(String[] args) {
